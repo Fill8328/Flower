3d5a9d1352c5780ac4142b98c8757e5c9a426d26
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-06-14 11:35:49 +0300

    Flower

--- src/fill/jma/Ex/Bouquet.java
+++ src/fill/jma/Ex/Bouquet.java
@@ -2,15 +2,5 @@ package fill.jma.Ex;
 
 public class Bouquet {
 
-    public void foo() {
 
-        Flower flower = new Flower();
-        flower.name = "Romachka";
-        flower.color = "White";
-
-        Flower flower1 = new Flower("Rose", "Red");
-
-        System.out.println(flower.color + " " + flower.name);
-        System.out.println(flower1.color + " " + flower1.name);
-    }
 }
--- src/fill/jma/Ex/Flower.java
+++ src/fill/jma/Ex/Flower.java
@@ -1,14 +1,64 @@
 package fill.jma.Ex;
 
 public class Flower {
-    String name;
-    String color;
 
-    public Flower(){}
+    private String name;
+    private String color;
+    private int longFlover;
+    private double cost;
+    private int freshnessLevel;
 
-    public Flower(String myName, String myColor){
-        name = myName;
+    public int getFreshnessLevel() {
+        return freshnessLevel;
+    }
+
+    public void setFreshnessLevel(int freshnessLevel) {
+        this.freshnessLevel = freshnessLevel;
+    }
+
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+
+    public Flower() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getLongFlover() {
+        return longFlover;
+    }
+
+    public void setLongFlover(int longFlover) {
+        this.longFlover = longFlover;
+    }
+
+    public Flower(String myFlower, String myColor, int myLongFlower, double myCost, int myFresh) {
+        name = myFlower;
         color = myColor;
+        longFlover = myLongFlower;
+        cost = myCost;
+        freshnessLevel = myFresh;
 
     }
 }
